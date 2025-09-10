f9d896f52e60e740ff68b8c521990afcf550a424
Sebastian <sebastian.pkfd@gmail.com>
2025-09-10 03:51:50 -0300

    HttpPoison installation

--- mix.exs
+++ mix.exs
@@ -65,7 +65,8 @@ defmodule Medicine.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:httpoison, "~> 2.0"}
     ]
   end
 
